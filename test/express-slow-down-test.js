b7866f96fbb2b5b91c1230540c12d720d57b4eae
Nathan Friedly <nathan.friedly@tanium.com>
2018-08-20 17:54:01 -0400

    drop rate-limiting support and a bit of legacy stuff

--- test/express-slow-down-test.js
+++ test/express-slow-down-test.js
@@ -7,11 +7,10 @@ var rateLimit = require("../lib/express-slow-down.js");
 // todo: look into using http://sinonjs.org/docs/#clock instead of actually letting the tests wait on setTimeouts
 
 describe("express-slow-down node module", function() {
-  var start, delay, message, app;
+  var start, delay, app;
 
   beforeEach(function() {
     start = Date.now();
-    message = "You have been very naughty.. No API response for you!!";
   });
 
   afterEach(function() {
@@ -23,7 +22,7 @@ describe("express-slow-down node module", function() {
     app.all("/", limit, function(req, res) {
       if (
         checkVar &&
-        req.rateLimit.limit === 5 &&
+        req.rateLimit.current === 1 &&
         req.rateLimit.remaining === 4
       ) {
         app.end(function(err, res) {
@@ -33,17 +32,7 @@ describe("express-slow-down node module", function() {
           return successHandler(null, res);
         });
       }
-
-      res.format({
-        html: function() {
-          res.send("response!");
-        },
-        json: function() {
-          res.json({
-            message: "response!"
-          });
-        }
-      });
+      res.send("response!");
     });
     // helper endpoint to know what ip test requests come from
     // set in headers so that I don't have to deal with the body being a stream
@@ -72,66 +61,16 @@ describe("express-slow-down node module", function() {
     };
   }
 
-  function goodRequest(
-    errorHandler,
-    successHandler,
-    key,
-    headerCheck,
-    limit,
-    remaining
-  ) {
+  function fastRequest(errorHandler, successHandler, key) {
     var req = request(app).get("/");
     // add optional key parameter
     if (key) {
       req = req.query({ key: key });
     }
 
-    if (headerCheck) {
-      req
-        .expect("x-ratelimit-limit", limit)
-        .expect("x-ratelimit-remaining", remaining)
-        .expect(function(res) {
-          if ("retry-after" in res.headers) {
-            throw new Error(
-              "Expected no retry-after header, got " +
-                res.headers["retry-after"]
-            );
-          }
-        })
-        .expect(200, /response!/)
-        .end(function(err, res) {
-          if (err) {
-            return errorHandler(err);
-          }
-          delay = Date.now() - start;
-          if (successHandler) {
-            successHandler(null, res);
-          }
-        });
-    } else {
-      req
-        .expect(200)
-        .expect(/response!/)
-        .end(function(err, res) {
-          if (err) {
-            return errorHandler(err);
-          }
-          delay = Date.now() - start;
-          if (successHandler) {
-            successHandler(null, res);
-          }
-        });
-    }
-  }
-
-  function goodJsonRequest(errorHandler, successHandler) {
-    request(app)
-      .get("/")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200, {
-        message: "response!"
-      })
+    req
+      .expect(200)
+      .expect(/response!/)
       .end(function(err, res) {
         if (err) {
           return errorHandler(err);
@@ -143,74 +82,8 @@ describe("express-slow-down node module", function() {
       });
   }
 
-  function badRequest(
-    errorHandler,
-    successHandler,
-    key,
-    headerCheck,
-    limit,
-    remaining,
-    retryAfter
-  ) {
-    var req = request(app).get("/");
-
-    // add optional key parameter
-    if (key) {
-      req = req.query({ key: key });
-    }
-
-    req = req.expect(429).expect(/Too many requests/);
-
-    if (headerCheck) {
-      req = req
-        .expect("retry-after", retryAfter)
-        .expect("x-ratelimit-limit", limit)
-        .expect("x-ratelimit-remaining", remaining);
-    }
-
-    req.end(function(err, res) {
-      if (err) {
-        return errorHandler(err);
-      }
-      delay = Date.now() - start;
-      if (successHandler) {
-        successHandler(null, res);
-      }
-    });
-  }
-
-  function badJsonRequest(errorHandler, successHandler) {
-    request(app)
-      .get("/")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(429, { message: "Too many requests, please try again later." })
-      .end(function(err, res) {
-        if (err) {
-          return errorHandler(err);
-        }
-        delay = Date.now() - start;
-        if (successHandler) {
-          successHandler(null, res);
-        }
-      });
-  }
-
-  function badRequestWithMessage(errorHandler, successHandler) {
-    request(app)
-      .get("/")
-      .expect(429)
-      .expect(message)
-      .end(function(err, res) {
-        if (err) {
-          return errorHandler(err);
-        }
-        delay = Date.now() - start;
-        if (successHandler) {
-          successHandler(null, res);
-        }
-      });
-  }
+  // for the moment, we're not checking the speed within the response. but this should make it easy to add that check later.
+  const slowRequest = fastRequest;
 
   it("should not allow the use of a store that is not valid", function(done) {
     try {
@@ -233,7 +106,7 @@ describe("express-slow-down node module", function() {
       })
     );
 
-    goodRequest(done, function() {
+    fastRequest(done, function() {
       if (!store.incr_was_called) {
         done(new Error("incr was not called on the store"));
       } else {
@@ -257,29 +130,9 @@ describe("express-slow-down node module", function() {
     }
   });
 
-  it("should send correct x-ratelimit-limit and x-ratelimit-remaining", function(done) {
-    createAppWith(rateLimit({ windowMs: 59100 }));
-    goodRequest(
-      done,
-      function(/* err, res */) {
-        delay = Date.now() - start;
-        if (delay > 99) {
-          done(new Error("First request took too long: " + delay + "ms"));
-        } else {
-          done();
-        }
-      },
-      undefined,
-      true,
-      "5",
-      "4",
-      "60"
-    );
-  });
-
   it("should allow the first request with minimal delay", function(done) {
     createAppWith(rateLimit());
-    goodRequest(done, function(/* err, res */) {
+    fastRequest(done, function(/* err, res */) {
       delay = Date.now() - start;
       if (delay > 99) {
         done(new Error("First request took too long: " + delay + "ms"));
@@ -295,12 +148,12 @@ describe("express-slow-down node module", function() {
         delayMs: 100
       })
     );
-    goodRequest(done, function(/* err, res */) {
+    fastRequest(done, function(/* err, res */) {
       if (delay > 99) {
         done(new Error("First request took too long: " + delay + "ms"));
       }
     });
-    goodRequest(done, function(/* err, res */) {
+    fastRequest(done, function(/* err, res */) {
       if (delay < 100) {
         return done(
           new Error("Second request was served too fast: " + delay + "ms")
@@ -319,10 +172,10 @@ describe("express-slow-down node module", function() {
         delayMs: 100
       })
     );
-    goodRequest(done);
-    goodRequest(done);
-    goodRequest(done);
-    goodRequest(done, function(/* err, res */) {
+    fastRequest(done);
+    fastRequest(done);
+    fastRequest(done);
+    fastRequest(done, function(/* err, res */) {
       // should be about 300ms delay on 4th request - because the multiplier starts at 0
       if (delay < 300) {
         return done(
@@ -343,17 +196,17 @@ describe("express-slow-down node module", function() {
         delayAfter: 2
       })
     );
-    goodRequest(done, function(/* err, res */) {
+    fastRequest(done, function(/* err, res */) {
       if (delay > 50) {
         done(new Error("First request took too long: " + delay + "ms"));
       }
     });
-    goodRequest(done, function(/* err, res */) {
+    fastRequest(done, function(/* err, res */) {
       if (delay > 100) {
         done(new Error("Second request took too long: " + delay + "ms"));
       }
     });
-    goodRequest(done, function(/* err, res */) {
+    fastRequest(done, function(/* err, res */) {
       if (delay < 100) {
         return done(
           new Error("Second request was served too fast: " + delay + "ms")
@@ -366,96 +219,27 @@ describe("express-slow-down node module", function() {
     });
   });
 
-  it("should allow delayAfter to be disabled entirely", function(done) {
-    createAppWith(
-      rateLimit({
-        delayMs: 1000,
-        delayAfter: 0
-      })
-    );
-    goodRequest(done);
-    goodRequest(done);
-    goodRequest(done);
-    goodRequest(done, function(/* err, res */) {
-      // should be about 300ms delay on 4th request - because the multiplier starts at 0
-      if (delay > 100) {
-        return done(
-          new Error("Fourth request was served too fast: " + delay + "ms")
-        );
-      }
-      done();
-    });
-  });
-
-  it("should refuse additional connections once IP has reached the max", function(done) {
-    createAppWith(
-      rateLimit({
-        delayMs: 0,
-        max: 2
-      })
-    );
-    goodRequest(done);
-    goodRequest(done);
-    badRequest(done, done);
-  });
-
-  it("should return the Retry-After header once IP has reached the max", function(done) {
-    createAppWith(
-      rateLimit({
-        delayMs: 0,
-        max: 1
-      })
-    );
-    goodRequest(done);
-    badRequest(done, done, undefined, true, "1", "0", "60");
-  });
-
-  it("should allow max to be disabled entirely", function(done) {
-    createAppWith(
-      rateLimit({
-        delayMs: 1,
-        max: 0
-      })
-    );
-    goodRequest(done);
-    goodRequest(done);
-    goodRequest(done, done);
-  });
-
-  it("should show the provided message instead of the default message when max connections are reached", function(done) {
-    createAppWith(
-      rateLimit({
-        delayMs: 0,
-        max: 2,
-        message: message
-      })
-    );
-    goodRequest(done);
-    goodRequest(done);
-    badRequestWithMessage(done, done);
-  });
-
-  it("should (eventually) accept new connections from a blocked IP", function(done) {
+  it("should (eventually) return to full speed", function(done) {
     createAppWith(
       rateLimit({
         delayMs: 100,
-        max: 2,
+        max: 1,
         windowMs: 50
       })
     );
-    goodRequest(done);
-    goodRequest(done);
-    badRequest(done);
+    fastRequest(done);
+    fastRequest(done);
+    slowRequest(done);
     setTimeout(function() {
       start = Date.now();
-      goodRequest(done, function(/* err, res */) {
+      fastRequest(done, function(/* err, res */) {
         if (delay > 50) {
           done(new Error("Eventual request took too long: " + delay + "ms"));
         } else {
           done();
         }
       });
-    }, 60);
+    }, 500);
   });
 
   it("should work repeatedly (issues #2 & #3)", function(done) {
@@ -467,20 +251,20 @@ describe("express-slow-down node module", function() {
       })
     );
 
-    goodRequest(done);
-    goodRequest(done);
-    badRequest(done);
+    fastRequest(done);
+    fastRequest(done);
+    slowRequest(done);
     setTimeout(function() {
       start = Date.now();
-      goodRequest(done, function(/* err, res */) {
+      fastRequest(done, function(/* err, res */) {
         if (delay > 50) {
           done(new Error("Eventual request took too long: " + delay + "ms"));
         } else {
-          goodRequest(done);
-          badRequest(done);
+          fastRequest(done);
+          slowRequest(done);
           setTimeout(function() {
             start = Date.now();
-            goodRequest(done, function(/* err, res */) {
+            fastRequest(done, function(/* err, res */) {
               if (delay > 50) {
                 done(
                   new Error("Eventual request took too long: " + delay + "ms")
@@ -493,23 +277,6 @@ describe("express-slow-down node module", function() {
         }
       });
     }, 60);
-  });
-
-  it("should allow the error statusCode to be customized", function(done) {
-    // note: node.js places some restrictions on what status codes are allowed
-    var errStatusCode = 456;
-    createAppWith(
-      rateLimit({
-        delayMs: 0,
-        max: 1,
-        statusCode: errStatusCode
-      })
-    );
-    goodRequest(done);
-    request(app)
-      .get("/")
-      .expect(errStatusCode)
-      .end(done);
   });
 
   it("should allow individual IP's to be reset", function(done) {
@@ -528,69 +295,14 @@ describe("express-slow-down node module", function() {
         if (!myIp) {
           return done(new Error("unable to determine local IP"));
         }
-        goodRequest(done);
-        badRequest(done, function(err) {
+        fastRequest(done);
+        slowRequest(done, function(err) {
           if (err) {
             return done(err);
           }
           limiter.resetIp(myIp);
-          goodRequest(done, done);
+          fastRequest(done, done);
         });
-      });
-  });
-
-  it("should respond with the default JSON object", function(done) {
-    var limiter = rateLimit({
-      delayMs: 0,
-      max: 1
-    });
-    createAppWith(limiter);
-    goodJsonRequest(done);
-    badJsonRequest(done, done);
-  });
-
-  it("should respond with text/html to browsers real Accept headers", function(done) {
-    var limiter = rateLimit({
-      delayMs: 0,
-      max: 1
-    });
-    createAppWith(limiter);
-    goodRequest(done);
-    request(app)
-      .get("/")
-      .set(
-        "Accept",
-        "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-      )
-      .expect(429, /Too many requests/)
-      .end(function(err) {
-        if (err) {
-          return done(err);
-        } else {
-          return done();
-        }
-      });
-  });
-
-  it("should use the custom handler when specified", function(done) {
-    var limiter = rateLimit({
-      delayMs: 0,
-      max: 1,
-      handler: function(req, res) {
-        res.status(429).end("Custom handler executed!");
-      }
-    });
-    createAppWith(limiter);
-    goodRequest(done);
-    request(app)
-      .get("/")
-      .expect(429, "Custom handler executed!")
-      .end(function(err) {
-        if (err) {
-          return done(err);
-        } else {
-          return done();
-        }
       });
   });
 
@@ -610,17 +322,17 @@ describe("express-slow-down node module", function() {
     });
 
     createAppWith(limiter);
-    goodRequest(done, null, 1);
-    goodRequest(done, null, 1);
-    goodRequest(done, null, 2);
-    badRequest(
+    fastRequest(done, null, 1);
+    fastRequest(done, null, 1);
+    fastRequest(done, null, 2);
+    slowRequest(
       done,
       function(err) {
         if (err) {
           return done(err);
         }
-        goodRequest(done, null, 2);
-        badRequest(done, done, 2);
+        fastRequest(done, null, 2);
+        slowRequest(done, done, 2);
       },
       1
     );
@@ -639,12 +351,12 @@ describe("express-slow-down node module", function() {
     });
 
     createAppWith(limiter);
-    goodRequest(done, null, 1);
-    goodRequest(done, null, 1);
-    goodRequest(done, done, 1); // 3rd request would normally fail but we're skipping it
+    fastRequest(done, null, 1);
+    fastRequest(done, null, 1);
+    fastRequest(done, done, 1); // 3rd request would normally fail but we're skipping it
   });
 
-  it("should pass current hits and limit hits to the next function", function(done) {
+  it("should pass current hits and remaining hits to the next function", function(done) {
     var limiter = rateLimit({
       headers: false
     });
