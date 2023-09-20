19fc4ab7227c8dac2df2eb5da6d3f04440967b36
Nathan Friedly <nathan@nfriedly.com>
2023-09-20 09:07:08 -0400

    req-object test passes

--- test/req-object.test.ts
+++ test/req-object.test.ts
@@ -1,5 +1,13 @@
+import {
+	describe,
+	expect,
+	beforeEach,
+	afterEach,
+	jest,
+	it,
+} from '@jest/globals'
 import slowDown from '../source/express-slow-down'
-import { expectNoDelay } from './helpers/requests.js'
+import { expectNoDelay } from './helpers/requests'
 
 describe('req augmentation', () => {
 	beforeEach(() => {
@@ -12,17 +20,17 @@ describe('req augmentation', () => {
 	})
 
 	it('should augment the req object with info about the slowdown status', async () => {
-		const request = {}
+		const request: any = {}
 		const instance = slowDown({
-			delayAfter: 1,
+			delayAfter: 2,
 			windowMs: 1000,
 		})
 		await expectNoDelay(instance, request)
 		expect(request.slowDown).toMatchObject({
 			current: 1,
 			delay: 0,
-			limit: 1,
-			remaining: 0,
+			limit: 2,
+			remaining: 1,
 			// ResetTime: Date object
 		})
 	})
