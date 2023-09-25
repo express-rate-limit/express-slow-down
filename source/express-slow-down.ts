c60f4dfd7e3d26ea6b222beb269fdbda4005e5d7
Vedant K <gamemaker0042@gmail.com>
2023-09-25 21:51:13 +0530

    chore: add comments to `source/`

--- source/express-slow-down.ts
+++ source/express-slow-down.ts
@@ -1,84 +1,152 @@
+// /source/express-slow-down.ts
+// The speed limiting middleware.
+
 import type { Request, Response, NextFunction } from 'express'
-import { rateLimit } from 'express-rate-limit'
-import type { SlowDownRequestHandler, AugmentedRequest, Options } from './types'
+import { rateLimit, type Options as RateLimitOptions } from 'express-rate-limit'
+import type {
+	SlowDownRequestHandler,
+	AugmentedRequest,
+	SlowDownOptions,
+	Options,
+} from './types'
 
-export function slowDown(
-	options_: Partial<Options> = {},
-): SlowDownRequestHandler {
-	// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-	if (options_.headers || options_.legacyHeaders || options_.standardHeaders) {
-		throw new Error('express-slow-down headers option was removed in v2.0.0')
+/**
+ * Remove any options where their value is set to undefined. This avoids overwriting defaults
+ * in the case a user passes undefined instead of simply omitting the key.
+ *
+ * @param passedOptions {Options} - The options to omit.
+ *
+ * @returns {Options} - The same options, but with all undefined fields omitted.
+ *
+ * @private
+ */
+const omitUndefinedOptions = (
+	passedOptions: Partial<Options>,
+): Partial<Options> => {
+	const omittedOptions: Partial<Options> = {}
+
+	for (const k of Object.keys(passedOptions)) {
+		const key = k as keyof Options
+
+		if (passedOptions[key] !== undefined) {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+			omittedOptions[key] = passedOptions[key]
+		}
 	}
 
-	if (options_.max !== undefined || options_.limit !== undefined) {
+	return omittedOptions
+}
+
+/**
+ * Create an instance of middleware that slows down responses to Express requests.
+ *
+ * @param passedOptions {Options} - Options to configure the speed limiter.
+ *
+ * @returns {SlowDownRequestHandle} - The middleware that speed-limits clients based on your configuration.
+ *
+ * @public
+ */
+export const slowDown = (
+	passedOptions: Partial<Options> = {},
+): SlowDownRequestHandler => {
+	// Passing undefined should be equivalent to not passing an option at all, so we'll
+	// omit all fields where their value is undefined.
+	const notUndefinedOptions: Partial<Options> =
+		omitUndefinedOptions(passedOptions)
+
+	if (
+		notUndefinedOptions.headers ||
+		notUndefinedOptions.legacyHeaders ||
+		notUndefinedOptions.standardHeaders
+	)
 		throw new Error(
-			'express-slow-down limit / max option is not supported, use delayAfter instead',
+			'The headers options were removed in express-slow-down v2.0.0.',
 		)
-	}
 
+	if (
+		notUndefinedOptions.max !== undefined ||
+		notUndefinedOptions.limit !== undefined
+	)
+		throw new Error(
+			'The limit/max option is not supported by express-slow-down, please use delayAfter instead.',
+		)
+
+	// Consolidate the validation options that have been passed by the user, and
+	// apply them later, along with `limit: false`.
 	const validate =
-		typeof options_.validate === 'boolean'
-			? { default: options_.validate }
-			: options_.validate
+		typeof notUndefinedOptions.validate === 'boolean'
+			? { default: notUndefinedOptions.validate }
+			: notUndefinedOptions.validate
 
-	const options = {
-		// Default settings that may be overridden
-		delayAfter: 1, // How many requests to allow through before starting to delay responses
+	// See ./types.ts#Options for a detailed description of the options and their
+	// defaults.
+	const options: Partial<RateLimitOptions> & SlowDownOptions = {
+		// The following settings are defaults that may be overridden by the user's options.
+		delayAfter: 1,
 		delayMs(used: number, request: AugmentedRequest, response: Response) {
-			const delayAfter = request[this.requestPropertyName].limit
+			const delayAfter = request[options.requestPropertyName!].limit
 			return (used - delayAfter) * 1000
-		}, // Number or function (may be async)
-		maxDelayMs: Number.POSITIVE_INFINITY, // Milliseconds - maximum delay to be applied to the response, regardless the request count. Infinity means that the delay will grow continuously and unboundedly
+		},
+		maxDelayMs: Number.POSITIVE_INFINITY,
 		requestPropertyName: 'slowDown',
 		validate: {
 			...validate,
 			limit: false, // We know the behavor of limit=0 changed - we depend on the new behavior!
 		},
-		// Additional options are passed directly to express-rate-limit
-		...options_,
-		// These settings cannot be overriden
-		limit: 0, // We want the handler to always run
+
+		// The following options are passed directly to `express-rate-limit`.
+		...notUndefinedOptions,
+
+		// These settings cannot be overriden.
+		limit: 0, // We want the handler to run on every request.
+		// Disable the headers, we don't want to send them.
 		legacyHeaders: false,
 		standardHeaders: false,
+		// The handler contains the slow-down logic, so don't allow it to be overriden.
 		async handler(_request: Request, response: Response, next: NextFunction) {
+			// Get the number of requests after which we should speed-limit the client.
 			const delayAfter =
 				typeof options.delayAfter === 'function'
 					? await options.delayAfter(_request, response)
 					: options.delayAfter
+
+			// Set the limit to that value, and compute the remaining requests as well.
 			const request = _request as AugmentedRequest
-			const info = request[options.requestPropertyName]
+			const info = request[options.requestPropertyName!]
 			info.limit = delayAfter
-			const { used } = info
-			info.remaining = Math.max(0, delayAfter - used)
+			info.remaining = Math.max(0, delayAfter - info.used)
+
+			// Compute the delay, if required.
 			let delay = 0
-			if (used > delayAfter) {
+			if (info.used > delayAfter) {
 				const unboundedDelay =
 					typeof options.delayMs === 'function'
-						? await options.delayMs(used, request, response)
+						? await options.delayMs(info.used, request, response)
 						: options.delayMs
 				const maxDelayMs =
 					typeof options.maxDelayMs === 'function'
 						? await options.maxDelayMs(request, response)
 						: options.maxDelayMs
+
+				// Make sure the computed delay does not exceed the max delay.
 				delay = Math.max(0, Math.min(unboundedDelay, maxDelayMs))
 			}
 
-			request[options.requestPropertyName].delay = delay
+			// Make sure the delay is also passed on with the request.
+			request[options.requestPropertyName!].delay = delay
 
-			if (delay <= 0) {
-				return next()
-			}
+			// If we don't need to delay the request, send it on its way.
+			if (delay <= 0) return next()
 
-			const timerId = setTimeout(() => {
-				next()
-			}, delay)
-			response.on('close', () => {
-				clearTimeout(timerId)
-			})
+			// Otherwise, set a timer and call `next` when the timer runs out.
+			const timerId = setTimeout(() => next(), delay)
+			response.on('close', () => clearTimeout(timerId))
 		},
 	}
 
+	// Create and return the special rate limiter.
 	return rateLimit(options)
 }
 
+// Export it to the world!
 export default slowDown
--- source/types.ts
+++ source/types.ts
@@ -1,42 +1,111 @@
+// /source/types.ts
+// All the types used by this package
+
 import type { Response } from 'express'
 import type {
-	AugmentedRequest as ERLAugmentedRequest,
+	AugmentedRequest as RateLimitedRequest,
 	RateLimitInfo,
-	Options as ERLOptions,
+	Options as RateLimitOptions,
 	ValueDeterminingMiddleware,
 	RateLimitRequestHandler,
 } from 'express-rate-limit'
 
+/**
+ * A modified Express request handler with the rate limit and slow down methods.
+ */
 export type SlowDownRequestHandler = RateLimitRequestHandler
 
-export type SlowDownInfo = RateLimitInfo & {
-	delay: number
-}
-export type AugmentedRequest = ERLAugmentedRequest & {
-	[key: string]: SlowDownInfo
-}
-
+/**
+ * Method to generate the delay to apply to the incoming request.
+ *
+ * @param used {number} - The number of requests made by the client so far.
+ * @param request {Request} - The Express request object.
+ * @param response {Response} - The Express response object.
+ *
+ * @returns {number} - The delay to apply.
+ */
 export type DelayFn = (
 	used: number,
 	request: AugmentedRequest,
 	response: Response,
 ) => number | Promise<number>
 
-export type Options = ERLOptions & {
-	// Express-slow-down specific options
-	delayAfter: number | ValueDeterminingMiddleware<number>
-	delayMs: number | DelayFn
-	maxDelayMs: number | ValueDeterminingMiddleware<number>
-
-	// Headers are not supported
+/**
+ * Options present in `express-rate-limit` that this package overrides.
+ */
+export type OverridenOptions = {
+	/**
+	 * The header options are not supported, and using them will throw an error.
+	 */
 	headers?: false
-	lefacyHeaders?: false
+	legacyHeaders?: false
 	standardHeaders?: false
 
-	// Limit / max is set from delayAfter
+	/**
+	 * The `limit` option is set from the handler using `delayAfter`.
+	 */
 	limit: never
 	max: never
 
-	// Handler is set by this library
+	/**
+	 * The `handler` option is overriden by the library.
+	 */
 	handler: never
+}
+
+/**
+ * All the `express-slow-down` specific options.
+ */
+export type SlowDownOptions = {
+	/**
+	 * The max number of requests allowed during windowMs before the middleware
+	 * starts delaying responses.
+	 *
+	 * Can be the limit itself as a number or a (sync/async) function that accepts
+	 * the Express req and res objects and then returns a number.
+	 *
+	 * Defaults to 1.
+	 */
+	delayAfter: number | ValueDeterminingMiddleware<number>
+
+	/**
+	 * The delay to apply to each request once the limit is reached.
+	 *
+	 * Can be the limit itself as a number or a (sync/async) function that accepts
+	 * the Express req and res objects and then returns a number.
+	 *
+	 * By default, it increases the delay by 1 second for every request over the limit.
+	 */
+	delayMs: number | DelayFn
+
+	/**
+	 * The absolute maximum value for delayMs. After many consecutive attempts,
+	 * the delay will always be this value. This option should be used especially
+	 * when your application is running behind a load balancer or reverse proxy
+	 * that has a request timeout.
+	 *
+	 * Defaults to infinity.
+	 */
+	maxDelayMs: number | ValueDeterminingMiddleware<number>
+}
+
+/**
+ * The configuration options for the middleware.
+ */
+export type Options = RateLimitOptions & OverridenOptions & SlowDownOptions
+
+/**
+ * The extended request object that includes information about the client's
+ * rate limit and delay.
+ */
+export type AugmentedRequest = RateLimitedRequest & {
+	[key: string]: SlowDownInfo
+}
+
+/**
+ * The rate limit and delay related information for each client included in the
+ * Express request object.
+ */
+export type SlowDownInfo = RateLimitInfo & {
+	delay: number
 }
